8bd92d94f9fc78e15adf260930f8a1eadfca8347
Addisu-Etana <addisuelectrical@gmail.com>
2025-12-13 14:19:03 +0300

    update image and animation text

--- script.js
+++ script.js
@@ -32,14 +32,16 @@ const typed = new Typed(".multiple-text", {
   backDelay: 1500,
   loop: true,
 });
-// const typed2 = new Typed('.Animation-text', {
-//   strings: [
-//     "Ethiopia",
-//     "Addis Ababa",
-
-//   ],
-//   typeSpeed: 80,
-//   backSpeed: 80,
-//   backDelay: 1500,
-//   loop: true,
-// });
+const typed2 = new Typed(".animation-text", {
+  strings: [
+    "Frontend Developer",
+    "React Developer",
+    "Javascript Developer",
+    "Web Designer",
+    "Technology Enthusiast",
+  ],
+  typeSpeed: 80,
+  backSpeed: 80,
+  backDelay: 1500,
+  loop: true,
+});
